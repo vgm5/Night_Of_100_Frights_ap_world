afa0826fc2133f3c3b7a8e9f345d0bde3d0d12d0
vgm5 <110358490+vgm5@users.noreply.github.com>
2024-03-08 20:55:33 -0600

    Updated LevelNames and RegionNames
    Replaced Sponge logic with Scooby logic

--- names/RegionNames.py
+++ names/RegionNames.py
@@ -3,78 +3,115 @@ from . import LevelNames
 menu = "Menu"
 
 # Hub
-pineapple = "Pineapple"
-hub1 = f"{LevelNames.hub}1"
-squid = "Squidward"
-pat = "Patrick"
-hub2 = f"{LevelNames.hub}2"
-tree = "Tree Dome"
-shoals = "Shoals"
-police = "Police"
-hub3 = f"{LevelNames.hub}3"
-kk = "Krusty Krab"
-cb = "Chum Bucket"
+hub1 = "Mystery Machine"
+hub2 = "Mystic Playground"
+hub3 = "Monster Gallery"
 
-# JF
-jf01 = f"{LevelNames.jf_short}01"
-jf02 = f"{LevelNames.jf_short}02"
-jf03 = f"{LevelNames.jf_short}03"
-jf04 = f"{LevelNames.jf_short}04"
 
-# BB
-bb01 = f"{LevelNames.bb_short}01"
-bb02 = f"{LevelNames.bb_short}02"
-bb03 = f"{LevelNames.bb_short}03"
-bb04 = f"{LevelNames.bb_short}04"
+# B
+b001 = f"{LevelNames.b_short}01"
+b002 = f"{LevelNames.b_short}02"
+b003 = f"{LevelNames.b_short}03"
+b004 = f"{LevelNames.b_short}04"
 
-# GL
-gl01 = f"{LevelNames.gl_short}01"
-gl02 = f"{LevelNames.gl_short}02"
-gl03 = f"{LevelNames.gl_short}03"
+# C
+c001 = f"{LevelNames.c_short}01"
+c002 = f"{LevelNames.c_short}02"
+c003 = f"{LevelNames.c_short}03"
+c004 = f"{LevelNames.c_short}04"
+c005 = f"{LevelNames.c_short}05"
+c006 = f"{LevelNames.c_short}06"
+c007 = f"{LevelNames.c_short}07"
 
-# B1
-b1 = LevelNames.b1_short
+# E
+e001 = f"{LevelNames.e_short}01"
+e002 = f"{LevelNames.e_short}02"
+e003 = f"{LevelNames.e_short}03"
+e004 = f"{LevelNames.e_short}04"
+e005 = f"{LevelNames.e_short}05"
+e006 = f"{LevelNames.e_short}06"
+e007 = f"{LevelNames.e_short}07"
+e008 = f"{LevelNames.e_short}08"
+e009 = f"{LevelNames.e_short}09"
 
-# RB
-rb01 = f"{LevelNames.rb_short}01"
-rb02 = f"{LevelNames.rb_short}02"
-rb03 = f"{LevelNames.rb_short}03"
+# F
+f001 = f"{LevelNames.f_short}01"
+f003 = f"{LevelNames.f_short}02"
+f004 = f"{LevelNames.f_short}03"
+f005 = f"{LevelNames.f_short}04"
+f006 = f"{LevelNames.f_short}05"
+f007 = f"{LevelNames.f_short}06"
+f008 = f"{LevelNames.f_short}07"
+f009 = f"{LevelNames.f_short}08"
+f010 = f"{LevelNames.f_short}09"
 
-# BC
-bc01 = f"{LevelNames.bc_short}01"
-bc02 = f"{LevelNames.bc_short}02"
-bc03 = f"{LevelNames.bc_short}03"
-bc04 = f"{LevelNames.bc_short}04"
-bc05 = f"{LevelNames.bc_short}05"
+# G
+g001 = f"{LevelNames.g_short}01"
+g002 = f"{LevelNames.g_short}02"
+g003 = f"{LevelNames.g_short}03"
+g004 = f"{LevelNames.g_short}04"
+g005 = f"{LevelNames.g_short}05"
+g006 = f"{LevelNames.g_short}06"
+g007 = f"{LevelNames.g_short}07"
+g008 = f"{LevelNames.g_short}08"
+g009 = f"{LevelNames.g_short}09"
 
-# SM
-sm01 = f"{LevelNames.sm_short}01"
-sm02 = f"{LevelNames.sm_short}02"
-sm03 = f"{LevelNames.sm_short}03"
-sm04 = f"{LevelNames.sm_short}04"
+# I
+i001 = f"{LevelNames.i_short}01"
+i003 = f"{LevelNames.i_short}02"
+i004 = f"{LevelNames.i_short}03"
+i005 = f"{LevelNames.i_short}04"
+i006 = f"{LevelNames.i_short}05"
+i020 = f"{LevelNames.i_short}06"
+i021 = f"{LevelNames.i_short}07"
 
-# B2
-b2 = LevelNames.b2_short
+# L
+l011 = f"{LevelNames.l_short}01"
+l013 = f"{LevelNames.l_short}02"
+l014 = f"{LevelNames.l_short}03"
+l015 = f"{LevelNames.l_short}04"
+l017 = f"{LevelNames.l_short}05"
+l018 = f"{LevelNames.l_short}06"
+l019 = f"{LevelNames.l_short}07"
 
-# KF
-kf01 = f"{LevelNames.kf_short}01"
-kf02 = f"{LevelNames.kf_short}02"
-kf04 = f"{LevelNames.kf_short}04"
-kf05 = f"{LevelNames.kf_short}05"
+# O
+o001 = f"{LevelNames.o_short}01"
+o002 = f"{LevelNames.o_short}02"
+o003 = f"{LevelNames.o_short}03"
+o004 = f"{LevelNames.o_short}04"
+o005 = f"{LevelNames.o_short}05"
+o006 = f"{LevelNames.o_short}06"
+o008 = f"{LevelNames.o_short}08"
 
-# GY
-gy01 = f"{LevelNames.gy_short}01"
-gy02 = f"{LevelNames.gy_short}02"
-gy03 = f"{LevelNames.gy_short}03"
-gy04 = f"{LevelNames.gy_short}04"
+# P
+p001 = f"{LevelNames.p_short}01"
+p002 = f"{LevelNames.p_short}02"
+p003 = f"{LevelNames.p_short}03"
+p004 = f"{LevelNames.p_short}04"
+p005 = f"{LevelNames.p_short}05"
 
-# BD
-db01 = f"{LevelNames.db_short}01"
-db02 = f"{LevelNames.db_short}02"
-db03 = f"{LevelNames.db_short}03"
-db04 = f"{LevelNames.db_short}04"
-db05 = f"{LevelNames.db_short}05"
+# R
+r001 = f"{LevelNames.r_short}01"
+r003 = f"{LevelNames.r_short}02"
+r004 = f"{LevelNames.r_short}03"
+r005 = f"{LevelNames.r_short}04"
+r020 = f"{LevelNames.r_short}05"
+r021 = f"{LevelNames.r_short}06"
 
-# B3
-b3 = f"{LevelNames.b3_short}"
+# S
+s001 = f"{LevelNames.s_short}01"
+s002 = f"{LevelNames.s_short}02"
+s003 = f"{LevelNames.s_short}03"
+s004 = f"{LevelNames.s_short}04"
+s005 = f"{LevelNames.s_short}05"
+s006 = f"{LevelNames.s_short}06"
 
+# W
+w001 = f"{LevelNames.w_short}01"
+w021 = f"{LevelNames.w_short}02"
+w022 = f"{LevelNames.w_short}03"
+w023 = f"{LevelNames.w_short}04"
+w025 = f"{LevelNames.w_short}05"
+w026 = f"{LevelNames.w_short}06"
+w027 = f"{LevelNames.w_short}07"
+w028 = f"{LevelNames.w_short}08"
